acd87ec68a749779f5c66669fd407c8a3403ee7f
kishoreandra <keechu1008@gmail.com>
2021-03-16 23:09:56 +0530

    added video detail and video item

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onFormSubmit = async (term) => {
     const searchResults = await youtube.get("/search", {
       params: {
@@ -10,13 +13,37 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: searchResults.data.items });
+    this.setState({
+      videos: searchResults.data.items,
+      selectedVideo: searchResults.data.items[0],
+    });
   };
+
+  componentDidMount() {
+    this.onFormSubmit("cars");
+  }
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onFormSubmit} />I have{" "}
-        {this.state.videos.length} videos
+        <SearchBar onFormSubmit={this.onFormSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
